f5b948a0d52cd486a9ea231b196e00dae84ebf21
Brad Traversy <traversymedia@gmail.com>
2018-09-18 13:27:58 -0400

    Packages

---
+++ 03_packages/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"math"
+
+	"github.com/bradtraversy/go_crash_course/03_packages/strutil"
+)
+
+func main() {
+	fmt.Println(math.Floor(2.7))
+	fmt.Println(math.Ceil(2.7))
+	fmt.Println(math.Sqrt(16))
+	fmt.Println(strutil.Reverse("hello"))
+}
