bd3514cd074b49475c88d2c62677d18d7d966079
Brad Traversy <traversymedia@gmail.com>
2018-09-18 13:29:45 -0400

    Range

---
+++ 09_range/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	ids := []int{33, 76, 54, 23, 11, 2}
+
+	// Loop through ids
+	for i, id := range ids {
+		fmt.Printf("%d - ID: %d\n", i, id)
+	}
+
+	// Not using index
+	for _, id := range ids {
+		fmt.Printf("ID: %d\n", id)
+	}
+
+	// Add ids together
+	sum := 0
+	for _, id := range ids {
+		sum += id
+	}
+	fmt.Println("Sum", sum)
+
+	// Range with map
+	emails := map[string]string{"Bob": "bob@gmail.com", "Sharon": "sharon@gmail.com"}
+
+	for k, v := range emails {
+		fmt.Printf("%s: %s\n", k, v)
+	}
+
+	for k := range emails {
+		fmt.Println("Name: " + k)
+	}
+}
