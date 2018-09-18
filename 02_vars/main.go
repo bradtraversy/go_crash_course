144b9d54b6e1dcd6c317434f52ea2b93898ae833
Brad Traversy <traversymedia@gmail.com>
2018-09-18 13:27:40 -0400

    Variables

---
+++ 02_vars/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	// MAIN TYPES
+	// string
+	// bool
+	// int
+	// int  int8  int16  int32  int64
+	// uint uint8 uint16 uint32 uint64 uintptr
+	// byte - alias for uint8
+	// rune - alias for int32
+	// float32 float64
+	// complex64 complex128
+
+	// Using var
+	// var name = "Brad"
+	var age int32 = 37
+	const isCool = true
+	var size float32 = 2.3
+
+	// Shorthand
+	// name := "Brad"
+	// email := "brad@gmail.com"
+
+	name, email := "Brad", "brad@gmail.com"
+
+	fmt.Println(name, age, isCool, email)
+	fmt.Printf("%T\n", size)
+}
