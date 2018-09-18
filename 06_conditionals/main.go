cddf0767e0390c5d4109b191417735c834b4146e
Brad Traversy <traversymedia@gmail.com>
2018-09-18 13:28:59 -0400

    Conditionals

---
+++ 06_conditionals/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 10
+	y := 10
+
+	//  If else
+	if x <= y {
+		fmt.Printf("%d is less than or equal to %d\n", x, y)
+	} else {
+		fmt.Printf("%d is less than %d\n", y, x)
+	}
+
+	// else if
+	color := "red"
+
+	if color == "red" {
+		fmt.Println("color is red")
+	} else if color == "blue" {
+		fmt.Println("color is blue")
+	} else {
+		fmt.Println("color is NOT blue or red")
+	}
+
+	// Switch
+	switch color {
+	case "red":
+		fmt.Println("color is red")
+	case "blue":
+		fmt.Println("color is blue")
+	default:
+		fmt.Println("color is NOT blue or red")
+	}
+
+}
