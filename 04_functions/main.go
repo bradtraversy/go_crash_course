572027cd6765de3c1231d8dcb155415be56077fe
Brad Traversy <traversymedia@gmail.com>
2018-09-18 13:28:16 -0400

    Functions

---
+++ 04_functions/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func greeting(name string) string {
+	return "Hello " + name
+}
+
+func getSum(num1, num2 int) int {
+	return num1 + num2
+}
+
+func main() {
+	fmt.Println(getSum(3, 4))
+}
