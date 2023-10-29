b0c976df9300cf0ce2d31f5901f21bc946de44f3
Eric <developer.ericgitangu@gmail.com>
2023-10-28 22:14:03 -0500

    Using the make construct

--- 08_maps/main.go
+++ 08_maps/main.go
@@ -4,23 +4,39 @@ import "fmt"
 
 func main() {
 	// Define map
-	// emails := make(map[string]string)
+	emails := make(map[string]string)
 
-	//  Assign kv
-	// emails["Bob"] = "bob@gmail.com"
-	// emails["Sharon"] = "sharon@gmail.com"
-	// emails["Mike"] = "mike@gmail.com"
-
-	// Decalre map and add kv
-	emails := map[string]string{"Bob": "bob@gmail.com", "Sharon": "sharon@gmail.com"}
-
+	// Add key-value pairs to map
+	emails["Bob"] = "bob@gmail.com"
+	emails["Sharon"] = "sharon@gmail.com"
 	emails["Mike"] = "mike@gmail.com"
 
+	// Print map
 	fmt.Println(emails)
+
+	// Print map length
 	fmt.Println(len(emails))
+
+	// Print value of a key
 	fmt.Println(emails["Bob"])
 
 	// Delete from map
 	delete(emails, "Bob")
 	fmt.Println(emails)
+
+	// Declare map and add key-value pairs
+	emails2 := map[string]string{"Bob": "bob@gmail.com", "Sharon": "sharon@gmail.com", "Mike": "mike@gmail.com"}
+	fmt.Println(emails2)
+
+	// Print map length
+	fmt.Println(len(emails2))
+
+	// Print value of a key
+	fmt.Println(emails2["Bob"])
+
+	// Delete from map
+	delete(emails2, "Bob")
+	fmt.Println(emails2)
+
+
 }
