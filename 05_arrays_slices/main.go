73630ca068a80ba5ba28d6cce741efdaaee05a5c
Eric <developer.ericgitangu@gmail.com>
2023-10-28 22:38:15 -0500

    Add comments for clarity

--- 05_arrays_slices/main.go
+++ 05_arrays_slices/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 )
 
+// main is the entry point for the program
 func main() {
 	// Arrays
 	// var fruitArr [2]string
@@ -18,17 +19,25 @@ func main() {
 	// fmt.Println(fruitArr)
 	// fmt.Println(fruitArr[1])
 
+	// Slice of strings
 	fruitSlice := []string{"Apple", "Orange", "Grape", "Cherry"}
+
+	// Print length of slice
 	fmt.Println(len(fruitSlice))
+
+	// Print a slice of the slice
 	fmt.Println(fruitSlice[1:3])
 
+	// Create a slice with make
 	fruitSlice2 := make([]string, 4)
 	fruitSlice2[0] = "Apple"
 	fruitSlice2[1] = "Orange"
 	fruitSlice2[2] = "Grape"
 	fruitSlice2[3] = "Cherry"
 
+	// Print the slice
 	fmt.Println(fruitSlice2)
-	fmt.Println(fruitSlice2[1:3])
 
+	// Print a slice of the slice
+	fmt.Println(fruitSlice2[1:3])
 }
