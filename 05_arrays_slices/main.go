ad5d887b9ce65be6411028e7b758a7890178d044
Eric <developer.ericgitangu@gmail.com>
2023-10-28 22:24:03 -0500

    make construct in slices

--- 05_arrays_slices/main.go
+++ 05_arrays_slices/main.go
@@ -19,7 +19,16 @@ func main() {
 	// fmt.Println(fruitArr[1])
 
 	fruitSlice := []string{"Apple", "Orange", "Grape", "Cherry"}
-
 	fmt.Println(len(fruitSlice))
 	fmt.Println(fruitSlice[1:3])
+
+	fruitSlice2 := make([]string, 4)
+	fruitSlice2[0] = "Apple"
+	fruitSlice2[1] = "Orange"
+	fruitSlice2[2] = "Grape"
+	fruitSlice2[3] = "Cherry"
+
+	fmt.Println(fruitSlice2)
+	fmt.Println(fruitSlice2[1:3])
+
 }
